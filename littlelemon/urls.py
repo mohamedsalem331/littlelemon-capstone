a511c51b149acb33e62ef60a04f48533fe088711
Mohamed Salem <simulator_2004@hotmail.com>
2024-07-17 12:06:22 +0300

    fix: headers href

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include("restaurant.urls")),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken'))
+    path("", include("restaurant.urls")),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
 ]
---
+++ restaurant/urls.py
@@ -0,0 +1,13 @@
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("menu/", views.menu, name="menu"),
+    path("menu_item/<int:pk>/", views.single_menu_item, name="menu_item"),
+    path("book/", views.book, name="book"),
+    # path('api-token-auth/', obtain_auth_token)
+]
